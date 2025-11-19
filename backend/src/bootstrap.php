6d00ca827e8ea316ec142d6055580428a66524b5
JoaoNetoNLC <joao@nlc.com>
2025-11-19 16:03:52 -0300

    fix: load env

--- backend/src/bootstrap.php
+++ backend/src/bootstrap.php
@@ -4,6 +4,23 @@ declare(strict_types=1);
 
 define('BASE_PATH', dirname(__DIR__));
 
+if (file_exists(BASE_PATH . '/.env')) {
+    $lines = file(BASE_PATH . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+        if (!array_key_exists($name, $_ENV)) {
+            putenv(sprintf('%s=%s', $name, $value));
+            $_ENV[$name] = $value;
+            $_SERVER[$name] = $value;
+        }
+    }
+}
+
 if (file_exists(BASE_PATH . '/vendor/autoload.php')) {
     require_once BASE_PATH . '/vendor/autoload.php';
 }
